2f4f1c71433027a3f08c738f452485d2d860c5f9
tianmingyangeric <43049612+tianmingyangeric@users.noreply.github.com>
2019-02-15 23:42:11 -0500

    Add files via upload

--- boat_rec.py
+++ boat_rec.py
@@ -7,12 +7,21 @@ import os
 import pandas as pd 
 from tempfile import TemporaryFile
 from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
 
 
 def image_feature_label(img,dir_name):
+	""" use hog to generate image features
+    Args:
+	     img : image name/image path
+	     dir_name: boat type folder, use to makde labels
+	Returns:
+        features[0]: feature array 
+        dif_name : label name
+        feature[1] : hig image 
+	"""
 	img = cv2.imread(img)
 	img = cv2.resize(img, (256, 256))
-	#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	features = ft.hog(img,  # input image
 	                  orientations=18,  # number of bins
 	                  pixels_per_cell=(16,16),  # pixel per cell
@@ -23,43 +32,111 @@ def image_feature_label(img,dir_name):
 	                  visualize=True)  # return HOG map
 	return features[0], dir_name, features[1]
 
-def generate_features():
+def generate_features(path):
+	""" generate all image features and labels
+    Args:
+	     path : path of training data folder and testing data folder
+	Returns:
+        feature and labnel array
+	"""
 	feature_list = []
 	label_list = []
-	dirpath = "./boat-types-recognition/"
-	dir_list = os.listdir(dirpath)
+	dirpath = path
+	dir_list = os.listdir(dirpath) # read all boat folders under training/testing folder
 	for dir_name in dir_list:
 		if not dir_name.startswith('.'):
 			folder_path = dirpath + dir_name + "/"
-			file_list = os.listdir(folder_path)
+			file_list = os.listdir(folder_path) # read all image  under boat type folder
 			for image in file_list:
 				if not image.startswith('.'):
 					image_path = folder_path + image
 					features, label, feature_image = image_feature_label(image_path,dir_name)
 					feature_list.append(features)
 					label_list.append(label)
-	return np.array([feature_list, label_list])
+	return np.array(feature_list),np.array(label_list)
 
 def save_feature_file():
-	features = generate_features()
-	np.save("type_feature",features)
+	""" 
+	save generated feature array to csv file
+	"""
+	training_features,training_label = generate_features("./training_data/")
+	test_features,test_label = generate_features("./test_data/")
+	np.save("training_data",training_features)
+	np.save("training_label",training_label)
+	np.save("test_data",test_features)
+	np.save("test_label",test_label)
 
-def load_feature_file():
-	feature,label = np.load("type_feature.npy")
+def load_training_file():
+	""" 
+	read generated feature array from csv file
+	"""
+	feature = np.load("training_data.npy")
+	label = np.load("training_label.npy")
+	return feature,label
+
+def load_testing_file():
+	feature = np.load("test_data.npy")
+	label = np.load("test_label.npy")
 	return feature,label
 
 
-#save_feature_file()
-#print ('xxx')
-feature,label = load_feature_file()
-#print (label)
 def PCA_Algorithm(data,d):
-	pca = PCA(n_components = 1)
+	'''used pca method to reduce features'''
+	pca = PCA(n_components = d)
 	pca.fit(data)
-	#print(pca.explained_variance_ratio_)
-	#print(pca.singular_values_)  
-PCA_Algorithm(feature,1)
+	final_data = pca.transform(data)
+	return final_data
 
+# best POV is 472
+def calculate_POV(data):
+    mean = np.mean(data,axis=0)
+    normal_data = data - mean
+    cov_matrix = np.cov(normal_data.T)  # calculate the cov
+    eig_value, eig_vector=  np.linalg.eig(cov_matrix)  
+    feature_rank = np.argsort(-eig_value) 
+    sum_eig =  sum(eig_value) * 0.95
+    count = 0
+    sum_count = 0
+    for index in feature_rank:
+    	if sum_count < sum_eig:
+    		sum_count += eig_value[index]
+    		count +=1
+    return count
+#save_feature_file()
+training_data, training_label = load_training_file()
+test_data, test_label = load_testing_file()
+
+def KNN_classifier(training_data, training_label, test_data, test_label, n):
+	neigh = KNeighborsClassifier(n_neighbors=n)
+	neigh.fit(training_data, training_label) 
+	test_result = neigh.predict(test_data)
+	test_score = neigh.score(test_data,test_label)
+	return test_result,test_score
+	#print(neigh.predict_proba([[0.9]]))
+''' 
+for n = 30, the accuracy is 0.4740
+for n = 11, the accuracy is 0.5240
+for n = 5, the accuracy is 0.49
+'''
+#result, score = KNN_classifier(training_data,training_label,test_data,test_label, 5)
+'''
+use pca first, then use knn classiffier
+let n = 11
+d = 10, score = 0.2406
+d = 5 , score = 0.28877
+'''
+#save_feature_file()
+training_data, training_label = load_training_file()
+test_data, test_label = load_testing_file()
+d = 20
+training_pca = PCA_Algorithm(training_data,d)
+testing_pca = PCA_Algorithm(test_data,d)
+result, score = KNN_classifier(training_pca,training_label,testing_pca,test_label, 5)
+print (result)
+print (score)
+#print (calculate_POV(feature))
+#final_data = PCA_Algorithm(feature,1)
+#print (final_data.shape)
 
 
 
